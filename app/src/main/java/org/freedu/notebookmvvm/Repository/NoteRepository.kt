9c17e127434cf31c29c2b624c4ac8d7fb59549d3
musfiqur552608 <mushfiqur15-10872@diu.edu.bd>
2024-05-09 11:13:57 +0600

    repository and viewmodel added

---
+++ app/src/main/java/org/freedu/notebookmvvm/Repository/NoteRepository.kt
@@ -0,0 +1,13 @@
+package org.freedu.notebookmvvm.Repository
+
+import org.freedu.notebookmvvm.database.NoteDatabase
+import org.freedu.notebookmvvm.model.Note
+
+class NoteRepository(private val db: NoteDatabase) {
+    suspend fun insertNote(note: Note) = db.getNoteDao().insertNote(note)
+    suspend fun deleteNote(note:Note) = db.getNoteDao().deleteNote(note)
+    suspend fun updateNote(note:Note) = db.getNoteDao().updateNote(note)
+
+    fun getAllNotes() = db.getNoteDao().getAllNotes()
+    fun searchNote(query:String) = db.getNoteDao().searchNote(query)
+}
---
+++ app/src/main/java/org/freedu/notebookmvvm/fragment/NewNoteFragment.kt
@@ -0,0 +1,22 @@
+package org.freedu.notebookmvvm.fragment
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import org.freedu.notebookmvvm.R
+
+
+class NewNoteFragment : Fragment() {
+
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_new_note, container, false)
+    }
+
+}
